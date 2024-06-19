9d02a2e892028c4eb86aa519a3f2b94ac8d75353
Tejas Soni <tejas@Tejass-MacBook-Pro.local>
2024-06-19 18:20:03 +0530

    working on users lambda

--- amplify/backend/function/usersLambda/src/app.js
+++ amplify/backend/function/usersLambda/src/app.js
@@ -6,87 +6,176 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  QueryCommand,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
 
+const ddbClient = new DynamoDBClient({ region: process.env.TABLE_REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+let TableName = "users";
+if (process.env.ENV && process.env.ENV !== "NONE") {
+  TableName = TableName + "-" + process.env.ENV;
+}
+
+const userIdPresent = false; // TODO: update in case is required to use that definition
+const partitionKeyName = "id";
+const partitionKeyType = "N";
+const sortKeyName = "updatedAt";
+const sortKeyType = "N";
+const hasSortKey = sortKeyName !== "";
+const path = "/meta-data";
+const UNAUTH = "UNAUTH";
+const hashKeyPath = "/:" + partitionKeyName;
+const sortKeyPath = hasSortKey ? "/:" + sortKeyName : "";
 
 // declare a new express app
-const app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
 
+// convert url string param to expected Type
+const convertUrlType = (param, type) => {
+  switch (type) {
+    case "N":
+      return Number.parseInt(param);
+    default:
+      return param;
+  }
+};
 
 /**********************
  * Example get method *
  **********************/
 
-app.get('/users', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await ddbClient.send(
+      new ScanCommand({
+        TableName,
+        Select: "ALL_ATTRIBUTES",
+      })
+    );
+    res.statusCode = users.$metadata.httpStatusCode;
+    res.json({
+      success: "get call succeed!",
+      data: {
+        data: users.Items,
+        total: users.Count,
+      },
+    });
+  } catch (error) {
+    res.json({ success: "Could not load users: " + error.message });
+  }
 });
 
-app.get('/users/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+app.get("/users/*", async function (req, res) {
+  const users = await ddbClient.send(
+    new ScanCommand({
+      TableName,
+      Select: "ALL_ATTRIBUTES",
+    })
+  );
+  res.statusCode = users.$metadata.httpStatusCode;
+  res.json({
+    success: "get call succeed!",
+    data: {
+      data: users.Items,
+      total: users.Count,
+    },
+  });
 });
 
 /****************************
-* Example post method *
-****************************/
+ * Example post method *
+ ****************************/
 
-app.post('/users', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+app.post("/users", async function (req, res) {
+  try {
+    const users = await ddbClient.send(
+      new ScanCommand({
+        TableName,
+        Select: "ALL_ATTRIBUTES",
+      })
+    );
+    console.log(users.Items);
+    // const currTimestamp = new Date().getTime()
+    // const createUserObject = {
+    //   id: users.Count,
+    //   ...req.body,
+    //   createdAt: currTimestamp,
+    //   updatedAt: currTimestamp
+    // };
+    // const user = await ddbClient.send(
+    //   new PutCommand({
+    //     TableName,
+    //     Item: createUserObject,
+    //   })
+    // );
+    // res.statusCode = user.$metadata.httpStatusCode;
+    res.json({
+      success: "post call succeed!",
+    });
+  } catch (error) {
+    res.json({ success: "Could not create users: " + error.message });
+  }
 });
 
-app.post('/users/*', function(req, res) {
+// app.post("/users/*", function (req, res) {
+//   // Add your code here
+//   res.json({ success: "post call succeed!", url: req.url, body: req.body });
+// });
+
+/****************************
+ * Example put method *
+ ****************************/
+
+app.put("/users", function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
+});
+
+app.put("/users/*", function (req, res) {
+  // Add your code here
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
 });
 
 /****************************
-* Example put method *
-****************************/
+ * Example delete method *
+ ****************************/
 
-app.put('/users', function(req, res) {
+app.delete("/users", function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: "delete call succeed!", url: req.url });
 });
 
-app.put('/users/*', function(req, res) {
+app.delete("/users/*", function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: "delete call succeed!", url: req.url });
 });
 
-/****************************
-* Example delete method *
-****************************/
-
-app.delete('/users', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/users/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.listen(3000, function() {
-    console.log("App started")
+app.listen(3000, function () {
+  console.log("App started");
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,6 +4,22 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
+    },
+    "users": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "portfolio829dc2b2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   },
   "function": {
@@ -13,6 +29,38 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
+    },
+    "metaData": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "usersLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "portfolio": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    },
+    "users": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
     }
   }
 }
